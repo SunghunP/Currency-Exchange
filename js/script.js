300d0ff09faf3da544e7d0ef1d0526a7ec587610
Joe Park <sunghun.josephp@gmail.com>
2022-03-26 14:15:27 -0700

    Add function to grab country codes and append them to select

--- js/script.js
+++ js/script.js
@@ -6,8 +6,18 @@ const $searchRight = $('#search-right')
 const $inputLeft = $('#input-left')
 const $display = $('#display')
 
+function getCodes(data, selection) {
+    for (let array of data) {
+        const $option = $('<option></option>')
+        $option.text(`${array[1]} (${array[0]})`)
+        $option.attr("value", `${array[0]}`)
+        $option.appendTo(selection)
+    }
+}
+
 $.ajax({
     url: `https://v6.exchangerate-api.com/v6/${API_KEY}/codes`})
 .then((data2) => {
-    console.log(data)
+    getCodes(data2.supported_codes, $searchLeft)
+    getCodes(data2.supported_codes, $searchRight)
 })
