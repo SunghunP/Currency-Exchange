47a18ad8d59e46e3357088009510e7fb040e070c
Joe Park <sunghun.josephp@gmail.com>
2022-03-26 14:14:28 -0700

    Add API call with AJAX to get country code values

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ const $searchLeft = $('#search-left')
 const $searchRight = $('#search-right')
 const $inputLeft = $('#input-left')
 const $display = $('#display')
+
+$.ajax({
+    url: `https://v6.exchangerate-api.com/v6/${API_KEY}/codes`})
+.then((data2) => {
+    console.log(data)
+})
