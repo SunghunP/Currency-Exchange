c82a70a807c35941e2c6f69feca7bc4f4b7313a9
Joe Park <sunghun.josephp@gmail.com>
2022-03-31 09:12:43 -0700

    Create new event listener for clicking of the select elements

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ myStorage = window.localStorage
 const $searchLeft = $('#search-left')
 const $searchRight = $('#search-right')
 const $inputLeft = $('#input-left')
+const $inputRight = $('#input-right')
 const $display = $('#display')
 
 // Function to get country codes from api and append them the html element you want. 
@@ -20,31 +21,39 @@ function getCodes(data, selection) {
     }
 }
 
-$.ajax({
-    url: `https://v6.exchangerate-api.com/v6/${API_KEY}/codes`})
-.then((data2) => {
-    getCodes(data2.supported_codes, $searchLeft)
-    getCodes(data2.supported_codes, $searchRight)
-})
+// $.ajax({
+//     url: `https://v6.exchangerate-api.com/v6/${API_KEY}/codes`})
+// .then((data2) => {
+//     getCodes(data2.supported_codes, $searchLeft)
+//     getCodes(data2.supported_codes, $searchRight)
+// })
 
-document.querySelector('button').addEventListener("click", () => {
+// depreciated event listener for convert button (no longer needed)
+// document.querySelector('button').addEventListener("click", () => {
+//     let selectleftVal = $searchLeft.val()
+//     let selectRightVal = $searchRight.val()
+//     let inputLeftVal = $inputLeft.val()
+//     // let inputRightVal = $inputRight.val()
+//     $.ajax({
+//         url: `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${selectleftVal}/${selectRightVal}`})
+//     .then((data) => {
+//         let rate = data.conversion_rate
+//         let final = inputLeftVal * rate.toFixed(2)
+//         $display.text(final)
+//     })
+// })
+
+document.querySelectorAll('select').forEach(select => select.addEventListener("change", () => {
     let selectleftVal = $searchLeft.val()
     let selectRightVal = $searchRight.val()
     let inputLeftVal = $inputLeft.val()
+    let inputRightVal = $inputRight.val()
     $.ajax({
         url: `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${selectleftVal}/${selectRightVal}`})
     .then((data) => {
-        const rate = data.conversion_rate
-        let final = inputLeftVal * rate
-        final.toFixed(2)
+        let rate = data.conversion_rate
+        let final = inputLeftVal * rate.toFixed(2)
         $display.text(final)
     })
-})
+}))
 
-// use restful country api to get country flags 
-// very useful since you can search up by currency.
-// https://restcountries.com/v3.1/currency/pen
-
-// Filter Response for restful country api 
-// You can filter the output of your request to include only the specified fields.
-// https://restcountries.com/v3.1/currency/pen?fields=capital only gets the capital field.
